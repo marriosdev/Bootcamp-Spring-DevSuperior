d25e2ccfa884cb7d56a7d421d4a0f54c80163676
edmariooliver <edmariooliver@outlook.com>
2022-04-26 22:57:42 -0300

    Subindo arquivos do projeto

---
+++ projeto_workshop/src/main/java/com/marrios/gcb/projeto_workshop/ProjetoWorkshopApplication.java
@@ -0,0 +1,13 @@
+package com.marrios.gcb.projeto_workshop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjetoWorkshopApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjetoWorkshopApplication.class, args);
+	}
+
+}
---
+++ projeto_workshop/src/test/java/com/marrios/gcb/projeto_workshop/ProjetoWorkshopApplicationTests.java
@@ -0,0 +1,13 @@
+package com.marrios.gcb.projeto_workshop;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjetoWorkshopApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
