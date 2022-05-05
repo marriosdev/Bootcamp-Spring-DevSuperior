53ff860c4968158e2301249178aad4a8146a18a7
edmariooliver <edmariooliver@outlook.com>
2022-05-05 00:45:20 -0300

    POST new category

--- projeto_workshop/src/main/java/com/marrios/gcb/projeto_workshop/resources/CategoryResources.java
+++ projeto_workshop/src/main/java/com/marrios/gcb/projeto_workshop/resources/CategoryResources.java
@@ -6,14 +6,18 @@ import com.marrios.gcb.projeto_workshop.services.CategoryService;
 import com.marrios.gcb.projeto_workshop.services.EntityNotFoundException;
 
 import  java.util.List;
+import java.net.URI;
 import  java.util.ArrayList;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 
 @RestController 
@@ -34,5 +38,13 @@ public class CategoryResources {
     public ResponseEntity<CategoryDTO> findById(@PathVariable Long id) {
         CategoryDTO dto = service.findById(id);        
         return ResponseEntity.ok().body(dto);
+    }
+
+    @PostMapping
+    public ResponseEntity<CategoryDTO> insert(@RequestBody CategoryDTO dto) {
+        dto = service.insert(dto);
+        URI uri = ServletUriComponentsBuilder.fromCurrentContextPath().path("/{id}")
+                    .buildAndExpand(dto.getId()).toUri();
+        return ResponseEntity.created(uri).body(dto);
     }
 }
