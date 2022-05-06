f90bcab79e0ab58aab1bf44a18b57c356235ff3f
Márrios <49793161+edmariooliver@users.noreply.github.com>
2022-05-06 00:51:19 -0300

    Delete projeto_workshop directory

--- projeto_workshop/src/main/java/com/marrios/gcb/projeto_workshop/services/exceptions/ResourceEntityNotFoundException.java
+++
@@ -1,9 +0,0 @@
-package com.marrios.gcb.projeto_workshop.services.exceptions;
-
-public class ResourceEntityNotFoundException extends RuntimeException{
-    private static final long  serialVersionUID = 1L;
-    
-    public ResourceEntityNotFoundException(String msg) {
-        super(msg);
-    }
-}
--- projeto_workshop/src/main/resources/import.sql
+++
@@ -1,3 +0,0 @@
-INSERT INTO tb_category (name) VALUES ('Books')
-INSERT INTO tb_category (name) VALUES ('Sports')
-INSERT INTO tb_category (name) VALUES ('Games')
